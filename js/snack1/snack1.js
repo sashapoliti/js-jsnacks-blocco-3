451d8ade3cec9df8098dd68ac026a1c044e62d7e
sashapoliti <sas.politi@gmail.com>
2024-03-14 11:58:45 +0100

    fix add control

--- js/snack1/snack1.js
+++ js/snack1/snack1.js
@@ -1,11 +1,15 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
 // Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
-let num = [];
+const num = [];
 let sum = 0;
+
 while (sum < 50) {
-    userNum = parseInt(prompt('Inserisci un numero'));
-    sum += userNum;
-    num.push(userNum);
-}; 
+    const userNum = parseInt(prompt('Inserisci un numero'));
+
+    if (!isNaN(userNum)) {
+        sum += userNum;
+        num.push(userNum);
+    }    
+}
 console.log(sum);
