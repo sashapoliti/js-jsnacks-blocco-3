bd5b9eaa9be7085321ff802f4768631362f2bcb6
sashapoliti <sas.politi@gmail.com>
2024-03-14 11:40:27 +0100

    complete

--- js/snack1/snack1.js
+++ js/snack1/snack1.js
@@ -2,6 +2,10 @@
 // Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
 let num = [];
+let sum = 0;
 while (sum < 50) {
-
-};
+    userNum = parseInt(prompt('Inserisci un numero'));
+    sum += userNum;
+    num.push(userNum);
+}; 
+console.log(sum);
