52bd2e8e2aafb3c8c89f0df0a6e0ef84e46ad8b5
sashapoliti <sas.politi@gmail.com>
2024-03-14 11:35:31 +0100

    init - structure

---
+++ js/snack1/snack1.js
@@ -0,0 +1,7 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+let num = [];
+while (sum < 50) {
+
+};
